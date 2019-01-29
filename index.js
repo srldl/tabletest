687b3ff825245eee14fd2324027525845b42cd99
srldl <siri.uldal@gmail.com>
2019-01-29 12:39:04 +0100

    prepped

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 'use strict';
-//var $;
-//$ = require('jquery');
+
+//var $ = require('jquery');
 //require('jquery-ui');
-//require('free-')
 
 
-//$(function () {
-  //  "use strict";
-  /*  $("#grid").jqGrid({
-        colModel: [
-            { name: "firstName" },
-            { name: "lastName" }
-        ],
-        data: [
-            { id: 10, firstName: "Angela", lastName: "Merkel" },
-            { id: 20, firstName: "Vladimir", lastName: "Putin" },
-            { id: 30, firstName: "David", lastName: "Cameron" },
-            { id: 40, firstName: "Barack", lastName: "Obama" },
-            { id: 50, firstName: "François", lastName: "Hollande" }
-        ]
-    }); */
+$.jgrid = $.jgrid || {};
+$.jgrid.no_legacy_api = true;
+$.jgrid.useJSON = true;
 
     $(function () {
        "use strict";
