97a2caa5d9e7d481b323076d945a9c3513d47da1
srldl <siri.uldal@gmail.com>
2019-02-06 15:07:12 +0100

    dataTable insert row works

--- index.js
+++ index.js
@@ -6,9 +6,50 @@ var dt = require( 'datatables.net' )( window, $ );
 //var buttons = require( 'datatables.net-buttons' )( window, $ );
 //var buttons2 = require( 'datatables.net-buttons-dt' )( window, $ );
 
+var dataSet = [
+      [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+      [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+      [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ]
+];
 
-console.log("test");
 
 $(document).ready(function() {
-    $('#example').DataTable();
+var table = $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+
+
+
+    console.log(table);
+    console.log($('#example').dataTable());
+    console.log( table.data() );
+
+    $('#saveBtn').click( function() {
+       console.log( table.data() );
+        return false;
+    } );
+
+    $('#addBtn').click( function() {
+       console.log( table.data() );
+       var rowNode = table
+       .row.add( [ 'Eva 2 Olsen',
+       '<td><input type="text" value=""></td>',
+       'Edinburgh', "8422", "2011/07/25", "$170,750"] )
+       .draw()
+       .node();
+
+       $( rowNode )
+       .css( 'color', 'red' )
+       .animate( { color: 'blue' } );
+
+        return false;
+    } );
 } );
