a11f50c8dc58f531533c7cb0d90c8649e2541a42
srldl <siri.uldal@gmail.com>
2019-02-07 13:06:20 +0100

    edit testing

--- index.js
+++ index.js
@@ -3,22 +3,25 @@
 var $  = require( 'jquery' );
 //require( 'datatables.net-dt' )( window, $ );
 var dt = require( 'datatables.net' )( window, $ );
-var select = require('datatables.net-select' )( window, $ );
+require('datatables.net-select' )( window, $ );
+//require('datatables.net-autofill' )( window, $ );
 //var buttons = require( 'datatables.net-buttons' )( window, $ );
 //var buttons2 = require( 'datatables.net-buttons-dt' )( window, $ );
 
 var dataSet = [
-      [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-      [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-      [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ]
+      [ "1","Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+      [  "2","Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+      [  "3","Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ]
 ];
 
 
 $(document).ready(function() {
 var table = $('#example').DataTable( {
         select: true,
+        autofill: true,
         data: dataSet,
         columns: [
+            { title: "ID" },
             { title: "Name" },
             { title: "Position" },
             { title: "Office" },
@@ -33,12 +36,11 @@ var table = $('#example').DataTable( {
     //console.log(table);
     //console.log($('#example').dataTable());
     //console.log( table.data() );
-  //  var tt = table.cell( ':eq(0)', null, {page: 'current'} ).select();
-  //  console.log(tt);
-  //  tt.focus();
 
     $('#saveBtn').click( function() {
-       console.log( table.data() );
+      var data = table.$('input, select').serialize();
+       //console.log(data[3].value);  //.substr( 0, 120 ));
+       console.log( data);
         return false;
     } );
 
@@ -63,7 +65,8 @@ var table = $('#example').DataTable( {
     } );
 
     $('#newBtn').click( function() {
-       var e1 = e2 = e3 = e4 = e5 = e6 = '<td><input type="text" value=""></td>';
+       var e1 = e3 = e4 = e5 = e6 = '<td><input type="text" name="fname"></td>';
+       var e2 = '<td><select name="car"><option value="volvo">Volvo</option><option value="saab">Saab</option></select></td>';
         var rowNode = table.row.add( [e1, e2, e3, e4, e5, e6] ).draw().node();
 
        $( rowNode )
@@ -71,6 +74,17 @@ var table = $('#example').DataTable( {
        .animate( { color: 'blue' } );
           console.log( table.data() );
 
+        return false;
+    } );
+
+    $('#editBtn').click( function() {
+         var rowNode = table.row('.selected');
+         console.log(rowNode.data());
+         var nodeArr =  rowNode.data();
+         //for (i=0;i<nodeArr.length;i++){
+        //  rowNode.remove().draw();
+          table.row('.selected').data(["3",'<td><input type="text" name="fname" value='+nodeArr[0]+'></td>', nodeArr[1],"e","e2","e3","e4"] ).draw().node();
+         //}
         return false;
     } );
 
