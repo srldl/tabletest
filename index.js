b655b601dc1d7495c9a4eb46175bf15b1fa3e525
srldl <siri.uldal@gmail.com>
2019-02-07 10:06:48 +0100

    new, copy, delete works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var $  = require( 'jquery' );
 //require( 'datatables.net-dt' )( window, $ );
 var dt = require( 'datatables.net' )( window, $ );
+var select = require('datatables.net-select' )( window, $ );
 //var buttons = require( 'datatables.net-buttons' )( window, $ );
 //var buttons2 = require( 'datatables.net-buttons-dt' )( window, $ );
 
@@ -15,6 +16,7 @@ var dataSet = [
 
 $(document).ready(function() {
 var table = $('#example').DataTable( {
+        select: true,
         data: dataSet,
         columns: [
             { title: "Name" },
@@ -28,28 +30,54 @@ var table = $('#example').DataTable( {
 
 
 
-    console.log(table);
-    console.log($('#example').dataTable());
-    console.log( table.data() );
+    //console.log(table);
+    //console.log($('#example').dataTable());
+    //console.log( table.data() );
+  //  var tt = table.cell( ':eq(0)', null, {page: 'current'} ).select();
+  //  console.log(tt);
+  //  tt.focus();
 
     $('#saveBtn').click( function() {
        console.log( table.data() );
         return false;
     } );
 
+    var e1 = 'Eva 2 Olsen';
+    var e2 = '<td><input type="text" value=""></td>';
+    var e3 = 'Edinburgh';
+    var e4 = "8422";
+    var e5 = "2011/07/25";
+    var e6 = "$170,750";
+
     $('#addBtn').click( function() {
-       console.log( table.data() );
-       var rowNode = table
-       .row.add( [ 'Eva 2 Olsen',
-       '<td><input type="text" value=""></td>',
-       'Edinburgh', "8422", "2011/07/25", "$170,750"] )
-       .draw()
-       .node();
+        console.log( e1);
+        var rowNode = table.row.add( [e1, e2, e3, e4, e5, e6] ).draw().node();
 
        $( rowNode )
        .css( 'color', 'red' )
        .animate( { color: 'blue' } );
 
+          console.log( table.data() );
+
+        return false;
+    } );
+
+    $('#newBtn').click( function() {
+       var e1 = e2 = e3 = e4 = e5 = e6 = '<td><input type="text" value=""></td>';
+        var rowNode = table.row.add( [e1, e2, e3, e4, e5, e6] ).draw().node();
+
+       $( rowNode )
+       .css( 'color', 'red' )
+       .animate( { color: 'blue' } );
+          console.log( table.data() );
+
+        return false;
+    } );
+
+    $('#delBtn').click( function() {
+       console.log(table.row($(this)));
+       var rowNode = table.row('.selected').remove().draw();
+       console.log( table.data() );
         return false;
     } );
 } );
