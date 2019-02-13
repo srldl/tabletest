f33fa79c27ef6c613aca617257895ddb64d7202e
srldl <siri.uldal@gmail.com>
2019-02-13 11:26:55 +0100

    fetch database data ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var $  = require( 'jquery' );
+var tb = require( '@srldl/edit-tabletest/js/edit-table.js');
 //require( 'datatables.net-dt' )( window, $ );
 var dt = require( 'datatables.net' )( window, $ );
 require('datatables.net-select' )( window, $ );
@@ -8,29 +9,94 @@ require('datatables.net-select' )( window, $ );
 //var buttons = require( 'datatables.net-buttons' )( window, $ );
 //var buttons2 = require( 'datatables.net-buttons-dt' )( window, $ );
 
-var dataSet = [
-      [ "1","Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
-      [  "2","Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
-      [  "3","Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ]
-];
+tb.printMsg();
+tb.testMsg();
 
+//Testdata  template and fieldwork
+let template = {
+  "_id": "a11a7305-45a8-4ad2-80d9-60f4b8980cc3",
+  "_rev": "8-99e39377c38e48d48b400c1e031b8a94",
+  "title": "Xxxdet var en gang",
+  "base": [
+    "project",
+    "parent_event_id",
+    "ris_id",
+    "rightsholder"
+  ],
+  "field": [
+    "matrix",
+    "event_date",
+    "latitude",
+    "longitude",
+    "placename",
+    "species",
+    "no_samples_amount",
+    "label_name"
+  ]
+};
+
+let fieldwork = [{
+  "matrix":"plasma",
+  "event_date": "1993-04-07T17:01:30Z",
+  "species": "ursus maritimus",
+  "latitude": 77,
+  "longitude": 16,
+  "placename": "Burgerbukta",
+  "no_samples_amount": "33"
+},
+{
+  "matrix":"plasma",
+  "event_date": "1999-04-07T17:01:30Z",
+  "species": "ursus maritimus",
+  "latitude": 77,
+  "longitude": 16,
+  "placename": "Ny-Ålesund",
+  "label_name": "35"
+}];
+
+
+//row is each displayed row in table
+let row = [];
+//dataSet is all rows displayed in a two dimensional array.
+let dataSet = [];
+//found is toggled to ensure all rows are filled in
+let found = false;
+//Traverse fieldwork array
+for (let n=0; n<fieldwork.length; n++) {
+  //Traverse template array to ensure correct headings with values
+  for (let value of template.field) {
+    found = false;
+    for (let m in fieldwork[n]) {
+           //If fieldwork object contains heading (template key)
+           if (m == value) {
+             //Push value into correct row/column
+             row.push(fieldwork[n][m]);
+             found = true;
+             //console.log(fieldwork[n][m]);
+           }
+    }
+    //If the method has no value/is not included, the value is empty string
+    if (found === false) {row.push("")};
+}
+//Push rows onto dataset, empty row array
+dataSet.push(row);
+row=[];
+}
+
+
+//Create table headings
+let columnsArr = [];
+for (let value of template.field) {
+  columnsArr.push({ 'title': value });
+}
 
 $(document).ready(function() {
 var table = $('#example').DataTable( {
         select: true,
         autofill: true,
         data: dataSet,
-        columns: [
-            { title: "ID" },
-            { title: "Name" },
-            { title: "Position" },
-            { title: "Office" },
-            { title: "Extn." },
-            { title: "Start date" },
-            { title: "Salary" }
-        ]
-    } );
-
+        columns: columnsArr
+});
 
 
     //console.log(table);
