c2257072f8090aca98b167d6330e1c208fca51ff
srldl <siri.uldal@gmail.com>
2019-03-26 11:11:08 +0100

    td id + select

--- index.js
+++ index.js
@@ -10,19 +10,15 @@ require( 'datatables.net-keytable' )( window, $ );
 
 
 //Testdata  template and fieldwork
-let template = {
-  "_id": "a11a7305-45a8-4ad2-80d9-60f4b8980cc3",
-  "_rev": "8-99e39377c38e48d48b400c1e031b8a94",
-  "title": "Det var en gang",
-  "field": [
+let template =  [
     "matrix",
     "project",
     "species",
     "species_identification",
     "label_name",
-    "comment"
-  ]
-};
+    "comment",
+    "id"
+];
 
 let fieldwork = [{
   "id":"4568140a7f01462edc029e42ab040f01",
@@ -31,7 +27,7 @@ let fieldwork = [{
   "species": "fulmarus glacialis",
   "species_identification": "77",
   "label_name": "6745232",
-  "comment": "Found dead"
+  "comment": "dead"
 },
 {
   "id":"4568140a7f01462edc029e42ab056e41",
@@ -53,76 +49,55 @@ let fieldwork = [{
 }];
 
 
-//row is each displayed row in table
-let index_count = 1;
-let row = [index_count++];
-//dataSet is all rows displayed in a two dimensional array.
-let dataSet = [];
-//found is toggled to ensure all rows are filled in
-let found = false;
-let id;
-//Traverse fieldwork array
-for (let n=0; n<fieldwork.length; n++) {
-  //Traverse template array to ensure matching headings and values
-  for (let value of template.field) {
-    found = false;
-    id = '';
-    for (let m in fieldwork[n]) {
-           //If fieldwork object contains heading (template key)
-           if (m == value) {
-             //Push value into correct row/column
-             row.push(fieldwork[n][m]);
-             found = true;
-             //Need to add ID at the end of the array
-           } else if (m == 'id') {
-              id  = fieldwork[n][m];
-           }
-    }
-    //If the method has no value/is not included, the value is empty string
-    if (found === false) {row.push('')};
-}
-//Push ID to array at the end
-row.push(id);
-
-//Push rows onto dataset, empty row array
-//First column is the listed index which should not be editable, hence skipped.
-for (let i=1; i< row.length; i++){
-   //If empty row, add a space so it can be accessed.
-   if (row[i] == '') { row[i] = '&nbsp;' }
-   row[i] = '<div contenteditable="true" style="color:black;background-color:white">' + row[i] + '</div>'
-};
-dataSet.push(row);
-row=[index_count++];
+let dataSet=[];
+let rowArr;
+//This counter holds max row_number
+let index_count = 0;
+function increment_index_count(){
+    index_count++;
 }
 
-//Create table headings
-let columnsArr = [{ 'title': 'index' }];
-for (let value of template.field) {
-  columnsArr.push({ 'title': value });
+//sort
+for (let j of fieldwork) {
+  rowArr = [""];
+  for (let k of template) {
+     let text = (j[k] == '') ? '&nbsp;' : j[k];
+     rowArr.push('<div contenteditable="true" id="'+ k +'_'+(index_count).toString()+'" style="color:black;background-color:white">' + text + '</div>');
+  }
+  //Id is the last entry, push directly without editable or id
+  rowArr[rowArr.length-1] = j['id'];
+  dataSet.push(rowArr);
+  increment_index_count();
 }
 
+  //Create table headings
+  let columnsArr = [{ 'title': 'index' }];
+  for (let value of template) {
+    columnsArr.push({ 'title': value });
+  }
 
+    let table;
+    $(document).ready(function() {
+      $('#table1').on( 'init.dt', function () {
+            table = $('#table1').DataTable();
+          } ).DataTable( {
 
-let table;
-$(document).ready(function() {
-  $('#table1').on( 'init.dt', function () {
-        table = $('#table1').DataTable();
-      } ).DataTable( {
-
-      stateSave: true,
-        data: dataSet,
-        columns: columnsArr,
-      keys: true,
-      autoFill: {
-        columns: ':not(:last-child)'
-    },
-        select:'single',
-        rowId: function(dataSet) {
-           return 'id_' + dataSet[8];
+          stateSave: true,
+            data: dataSet,
+            columns: columnsArr,
+        //  keys: true,
+          autoFill: {
+            columns: ':not(:last-child)'
         },
-        "ordering": true,
-      //  "columnDefs": [{targets: 2, type: 'formatted-num'}]
-    } );
+            select:'single',
+            keys: true,  //connected events key-blur
+          //  rowId:  ,
+          /*  rowId: function(index_count) {
+              console.log(index_count);
+              return   index_count;
+            }, */
+            "ordering": true
+        } );
 
     //Provide index
     table.on( 'order.dt search.dt', function () {
@@ -131,152 +106,176 @@ $(document).ready(function() {
            } );
        } ).draw();
 
-let index = 0;
-let old_index = 0;
+  add_id();
 
-table.on( 'user-select', function ( e, dt, type, cell, originalEvent ) {
-        //Get the new and the old index
-        old_index = index;
-        index = cell.index().row;
-
-        //Get row
-        var row = dt.row( cell.index().row ).node();
-        if ( $(row).hasClass('selected') ) {
-
-              // deselect
-              console.log("deselect");
-              return false;
-
-        } else {
-           console.log(dt);
-           update_last_row(dt, old_index);
-           //Get new values
-           //let row_content = dt.row( old_index ).node().innerHTML;
-           //Split string into array
-           //let row_content2 = row_content.replace(/<\/td>/g,"</td>,").split(",");
-           //Draw array
-           //let rowNode = table.row(old_index).data(row_content2).draw();
-        }
-
-    });
-
-//Update the last row edited
-var update_last_row = (dt, old_index) => {
-  //Get new values
-  let row_content = dt.row( old_index ).node().innerHTML;
-  //Split string into array
-  let row_content2 = row_content.replace(/<\/td>/g,"</td>,").split(",");
-  //Draw array
-  let rowNode = table.row(old_index).data(row_content2).draw(false);
-    //table.state.save();
-  //table.state.loaded();
-}
-
-
-
-$('#saveBtn').click( function() {
-   let sel_row = table.row({ selected: true });
-   update_last_row(sel_row, index);
-   var data = table.data();
-   console.log(data);
-   return false;
-} );
-
-$('#copyBtn').click( function() {
-
-    //Get updated text
-    if (!table.row({ selected: true }).node()) {
-        alert("Please select at least one row");
-    } else {
-        //First update row if the user have edited before
-        let sel_row2 = table.row({ selected: true });
-        update_last_row(sel_row2, index);
-
-        //Get hold of the content
-        let sel_row =   table.row({ selected: true }).node().innerHTML;
-        //Split string into array
-        let cpy_row = sel_row.replace(/<\/td>/g,"</td>,").split(",");
-          //If sel_row has id, remove it to become a new entry
-        let cpy_row2 = (cpy_row.slice(1,((template.field).length+1)));
-
-        //Get number of rows
-        let  index_counter = get_index();
-        let num_rows = get_rows();
-        let rowNode;
-
-        //Loop through to include the demanded copies
-        for (let j=0;j<(num_rows);j++){
-           //Make a deep copy
-           let cpy_row3 = [].concat(cpy_row2);
-           //Update index_counter
-          // cpy_row3.splice(0,0,(index_counter++).toString());
-            cpy_row3.splice(0,0,(index_counter++));
-           rowNode = table.row.add(cpy_row3).draw().node();
-        };
-
-        $( rowNode )
-                .animate( { color: 'blue' } );
-        }
-        return false;
-} );
-
-//Get the number of new or copied row we want
-//If null, return 1 for one row added
-var get_rows = () => {
-    let num_input = document.getElementById('addRows').value;
-    //Set value back to empty
-    document.getElementById('addRows').value = '';
-    let num_rows = num_input.match(/[0-9]+/);
-    if (num_rows !== null){
-      return num_rows[0];
-    } else {
-      return 1;
+  //Add id to <td> html element
+  function add_id () {
+    table.cells().nodes()[0]['id'] = "undefined_0";
+    for  (let b=1;b<table.cells().nodes().length;b++){
+        let obj = table.cells().nodes()[b]['_DT_CellIndex'];
+        table.cells().nodes()[b]['id'] = template[obj.column-1] + "_" + obj.row.toString();
     }
-}
-
-//Get index from the last row
-var get_index = () => {
-  let last_index = (table.rows().data().length);
-  let row_with_index = table.column(0).data()[last_index-1];
-  return parseInt(row_with_index)+1;
-}
+    return false;
+  }
 
 
+       //Get the number of new or copied row we want
+       //If null, return 1 for one row added
+       var get_rows = () => {
+           let num_input = document.getElementById('addRows').value;
+           //Set value back to empty
+           document.getElementById('addRows').value = '';
+           let num_rows = num_input.match(/[0-9]+/);
+           if (num_rows !== null){
+             return num_rows[0];
+           } else {
+             return 1;
+           }
+       }
 
-$('#newBtn').click( function() {
-        let sel;
-        let num_rows = get_rows();
-        let arr;
+       let index = 0;
+       let old_index = 0;
+       table.on( 'user-select', function ( e, dt, type, cell, originalEvent ) {
+               //Get the new and the old index
+               old_index = index;
+               index = cell.index().row;
+            //   table.row(cell.index().row).nodes()[0].id = "oo";
+               console.log(table.row(cell.index().row).nodes());
 
-        //Get number of rows
-        let  index_counter = get_index();
 
-        for (let j=0;j<(num_rows);j++){
-          arr = [index_counter++];
-          
+               //Get row
+               var row = dt.row( cell.index().row ).node();
 
-          template.field.forEach(function (i){
-             arr.push('<div contenteditable="true" style="color:black;background-color:white">&nbsp;</div>');
-          });
+               if ( $(row).hasClass('selected') ) {
+                     //deselect
+                     console.log("deselect");
+                    //  return false;
+               } else {
+                  //console.log(dt);
+                  update_last_row(dt, old_index);
+               }
+              // return false;
+               //If we have select menus they should appear now
+               implement_select_menu( template[(cell[0][0].column)-1] + "_" + (cell[0][0].row).toString()  );
+      });
 
-          sel = table.row.add( arr ).draw().node();
-        };
+      //Update the last row edited
+      var implement_select_menu = (id) => {
+        console.log(id);
+        id = "#"+id;
+        var parent = $(id).parent();
 
-        //Make the last entry active
-      //  $( rowNode ).animate( { color: 'blue' } );
-        return false;
-     });
-
-    $('#delBtn').click( function() {
-      let sel_row = table.row({ selected: true });
-      //Check that a row has been selected
-      if (sel_row[0].length === 0) {
-          alert("Please select at least one row");
-      } else {
-       var rowNode = table.row('.selected').remove().draw();
+            var returnstring = '';
+            let arr = ['feather','egg', 'liver'];
+            for (var i in arr) {
+                    returnstring += "<option value='" + arr[i] + "'>" + arr[i] + "</option>";
+         }
+         $(id).replaceWith("<td id='test'><div><select>" + returnstring + "</select></div></td>");
+      //   $(id).replaceWith("<td id='test'><div contenteditable=\"true\"><select>" + returnstring + "</select></div></td>");
+         //Remove select from where
       }
-       return false;
-    });
 
 
-  });
+       $('#copyBtn').click( function() {
+
+           //Get updated text
+           if (!table.row({ selected: true }).node()) {
+               alert("Please select at least one row");
+           } else {
+               //Get hold of the content
+               let sel_row =   table.row({ selected: true }).data();
+               let cpy_row = [];
+
+               //Get rid of old divs - extract innerHTML from text string
+               for (var c of sel_row){
+                    cpy_row.push(c.replace(/<\/*div[^>]*>/g,""));
+               }
+
+               let num_rows = get_rows();
+               let rowArr, rowNode;
+
+               //Get number of rows
+               for (let j=0;j<(num_rows);j++){
+                 rowArr = [""];
+
+                 for (let i=0;i<template.length;i++){
+                    rowArr.push('<div contenteditable="true" id="'+ template[i] +'_'+ index_count.toString()+'" style="color:black;background-color:white">'+cpy_row[i+1]+'</div>');
+                 };
+               //Id shouldnot have id or be editable
+                rowArr[rowArr.length-1] = '';
+                rowNode = table.row.add( rowArr ).draw().node();
+                rowNode.id = "row_"+index_count;
+                increment_index_count();
+               };
+               add_id();
+               $( rowNode )
+                       .animate( { color: 'blue' } );
+               }
+               return false;
+       } );
+
+       //Update the last row edited
+       var update_last_row = (dt, old_index) => {
+         //Get new values
+         let row_content = dt.row( old_index ).node().innerHTML;
+         //Change a long text string HTML style into an div array
+         //Only with node() we can get the updated value.
+         let row_content2 = row_content.replace(/<\/*td[^>]*>/g,"")
+                                       .replace(/<\/div>/g,"</div>,")
+                                       .replace(/[^<]/,",").split(",");
+         //Draw (publish) array
+         let rowNode = table.row(old_index).data(row_content2).draw(false);
+       }
+
+       $('#saveBtn').click( function() {
+          let sel_row = table.row({ selected: true }).nodes();
+          console.log(sel_row);
+          console.log(table.row({ selected: true }).data());
+
+          //table.cells().nodes()[0]['id'] = "no1";
+          //table.cells().nodes()[2]['_DT_CellIndex'];
+
+          update_last_row(sel_row, sel_row.index);
+
+
+          //console.log(table.cell({ selected: true }))
+
+          var data = table.data();
+          console.log(table);
+          return false;
+       } );
+
+
+       $('#newBtn').click( function() {
+               let num_rows = get_rows();
+               let rowArr;
+
+               //Get number of rows
+               for (let j=0;j<(num_rows);j++){
+                 rowArr = [""];
+
+                 template.forEach(function (i){
+                    rowArr.push('<div contenteditable="true" id="'+ i +'_'+ index_count.toString()+'" style="color:black;background-color:white">&nbsp;</div>');
+                 });
+               //Id should not be editable
+                rowArr[rowArr.length-1] = '';
+                let rowNode = table.row.add( rowArr ).draw().node();
+                //Add row id to TR element
+                rowNode.id = "row_"+index_count;
+                increment_index_count();
+                add_id();
+               };
+               return false;
+       });
+
+       $('#delBtn').click( function() {
+             let sel_row = table.row({ selected: true });
+             //Check that a row has been selected
+             if (sel_row[0].length === 0) {
+                 alert("Please select at least one row");
+             } else {
+              var rowNode = table.row('.selected').remove().draw();
+             }
+              return false;
+           });
+});
