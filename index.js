7c320a8b537d839eb9a2e092196c9af88b96d17c
srldl <siri.uldal@gmail.com>
2019-07-10 15:10:21 +0200

    copy/paste without select works

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 'use strict';
 
 //the rows
-let dataRows=[["A","AA","2019-06-14T12:00:00Z"],["B","BB","2019-06-13T12:00:00Z"],["C","CC","2019-06-14T12:00:00Z"]];
+let dataRows=[["A","AA","2019-06-14T12:00:00Z"],["B","BB","2019-06-13T12:00:00Z"],
+              ["C","CC","2019-06-14T12:00:00Z"],["D","DD","2019-07-17T12:00:00Z"]];
 
 //Create object with input parameters
 let obj =  {  "dataRows": dataRows,
               "headers": ["project", "subproject", "event_date"],
               "headers_tooltip": ["project letter","subproject letters","start date of work"],
-              "selectlist": {"project":["A","B","C"]},
+              "selectlist": {"project":["A","B","C","D"]},
               "autocompletes": ["subproject"],
               "dateFields":["event_date"],
               "id": "exceltable"
 };
 
-//Table width
+
+//Holds the width of the table
 let col_length = obj.headers.length;
 //This counter holds next row_number
 let row_length = 0;
+//Holds the array marked for copy
+let drag_arr = [];
 
 
 
 function input_element(id_td,id,inputValue,readOnly,backgroundColor,borderColor,fontWeight,fontSize){
   var td = document.createElement("td");
   td.id = id_td;
+//  td.ondragstart = addEventListener('drop', function(event) {
+  //  var data = event.dataTransfer.getData("text/plain");
+  //  event.target.value = data;
+//  });
+
   var input = document.createElement("input");
   input.type = "text";
   input.id = id;
@@ -40,7 +49,7 @@ function input_element(id_td,id,inputValue,readOnly,backgroundColor,borderColor,
 function new_row(row,no_columns){
   let tr1 = document.createElement("tr");
   for (let j=0;j<no_columns;j++){
-    let input1 = input_element('cell_'+row+'_'+j,'input_'+row+'_'+j,'',false,'white','white','normal','100%');
+    let input1 = input_element('td_'+row+'_'+j,'input_'+row+'_'+j,'',false,'white','white','normal','100%');
     tr1.appendChild(input1);
  }
  row_length++;
@@ -74,7 +83,7 @@ function th_element(id,textContent, textTooltip){
   for (let k=0;k<obj.dataRows.length;k++){
        let tr1 = document.createElement("tr");
        for (let j=0;j<obj.dataRows[0].length;j++){
-          let input1 = input_element('cell_'+k+'_'+j,'input_'+k+'_'+j,obj.dataRows[k][j],false,'white','white','normal','100%');
+          let input1 = input_element('td_'+k+'_'+j,'input_'+k+'_'+j,obj.dataRows[k][j],false,'white','white','normal','100%');
           tr1.appendChild(input1);
        }
        row_length++;
@@ -118,4 +127,95 @@ function th_element(id,textContent, textTooltip){
   }*/
 }
 
-  document.onkeydown = checkKey;
+document.addEventListener("dragover", function( event ) {
+      let id = event.target.id;
+      //Don't copy yet, add id to drag_arr - drag_arr should only contain same values in a row
+     if ((id.startsWith('td'))&&(id !== drag_arr[drag_arr.length-1])) {
+       //if user have returned back (regretting), skip last cell
+       if (id === drag_arr[drag_arr.length-2]) {
+         let last_id = drag_arr[drag_arr.length-1];
+         //Remove selected border
+         document.getElementById(last_id).classList.remove('dragCell');
+         drag_arr.pop();
+       } else {  //No user regrets, continue dragging
+         //Set a border so the user can see the cell is selected
+         document.getElementById(id).classList.add('dragCell');
+         drag_arr.push(id);
+
+       }
+     }
+ }, false);
+
+ document.addEventListener("dragend", function( event ) {
+    //If drop_arr last value is equal to first value, skip everything.
+    //The user has withdraw the action.
+    let drop_value = document.getElementById(event.target.id).childNodes[0].value;
+    for (var entry of drag_arr) {
+          let elem = document.getElementById(entry);
+          elem.classList.remove('dragCell');
+          if (drag_arr[drag_arr.length-1] !== event.target.id) {
+             elem.childNodes[0].value = drop_value;
+          }
+    }
+    //Reset drag_arr until next drag/drop
+    drag_arr = [];
+ }, false);
+
+ document.addEventListener("drop", function( event ) {
+   event.preventDefault();
+ }, false);
+
+
+ //Remove style borders from the whole table
+ function  remove_borders (){
+   for (let i=0;i<row_length;i++){
+      for (let j=0;j<col_length;j++){
+         document.getElementById('td_'+ i + '_' + j).classList.remove('selectCell');
+         document.getElementById('td_'+ i + '_' + j).classList.remove('dragCell');
+      }
+   }
+ }
+
+
+//Set cell and row select
+let handleClick = function (event) {
+  let doc = document.getElementById(event.explicitOriginalTarget.id);
+  let elem = doc.parentElement;
+  //Remove borders from the whole table
+  remove_borders();
+  //Mark selected cell
+  elem.classList.add("selectCell");
+  //Make it draggable
+  elem.draggable = "true";
+  elem.ondragstart = addEventListener('dragstart', function(event) { event.dataTransfer.setData('text/plain', doc.value); });
+}
+
+// new button pressed
+let newBtn = function (event) {
+    console.log('newBtn');
+};
+
+// copy button pressed
+let copyBtn = function (event) {
+    console.log('copyBtn');
+};
+
+// save button pressed
+let delBtn = function (event) {
+    console.log('delBtn');
+};
+
+// save button pressed
+let saveBtn = function (event) {
+    console.log('saveBtn');
+    container = document.getElementById("tbody1");
+    console.log(container);
+};
+
+
+document.onkeydown = checkKey;
+container.addEventListener('click', handleClick);
+document.getElementById("newBtn").addEventListener('click', newBtn);
+document.getElementById("copyBtn").addEventListener('click', copyBtn);
+document.getElementById("delBtn").addEventListener('click', delBtn);
+document.getElementById("saveBtn").addEventListener('click', saveBtn);
