7de98fbf88fac0a08c5c8443464252fdcb1f9b27
srldl <siri.uldal@gmail.com>
2019-07-11 12:43:22 +0200

    count+id cols added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 //the rows
-let dataRows=[["A","AA","2019-06-14T12:00:00Z"],["B","BB","2019-06-13T12:00:00Z"],
+let dataRows= [["A","AA","2019-06-14T12:00:00Z"],["B","BB","2019-06-13T12:00:00Z"],
               ["C","CC","2019-06-14T12:00:00Z"],["D","DD","2019-07-17T12:00:00Z"]];
 
 //Create object with input parameters
 let obj =  {  "dataRows": dataRows,
               "headers": ["project", "subproject", "event_date"],
-              "headers_tooltip": ["project letter","subproject letters","start date of work"],
+              "headers_tooltip": ["project acronym","subproject acronym","start work date"],
               "selectlist": {"project":["A","B","C","D"]},
               "autocompletes": ["subproject"],
               "dateFields":["event_date"],
@@ -16,9 +16,9 @@ let obj =  {  "dataRows": dataRows,
 
 
 //Holds the width of the table
-let col_length = obj.headers.length;
+let col_length = obj.headers.length + 2;
 //This counter holds next row_number
-let row_length = 0;
+let row_length = 1;
 //Holds the array marked for copy
 let drag_arr = [];
 
@@ -27,10 +27,6 @@ let drag_arr = [];
 function input_element(id_td,id,inputValue,readOnly,backgroundColor,borderColor,fontWeight,fontSize){
   var td = document.createElement("td");
   td.id = id_td;
-//  td.ondragstart = addEventListener('drop', function(event) {
-  //  var data = event.dataTransfer.getData("text/plain");
-  //  event.target.value = data;
-//  });
 
   var input = document.createElement("input");
   input.type = "text";
@@ -46,16 +42,22 @@ function input_element(id_td,id,inputValue,readOnly,backgroundColor,borderColor,
 }
 
 /* Create a new row */
-function new_row(row,no_columns){
+function new_row(no_columns){
   let tr1 = document.createElement("tr");
-  for (let j=0;j<no_columns;j++){
-    let input1 = input_element('td_'+row+'_'+j,'input_'+row+'_'+j,'',false,'white','white','normal','100%');
+  //Count column
+  tr1.appendChild(new_count_id('count_'+row_length,row_length));
+  //User columns
+  for (let j=1;j<no_columns-1;j++){
+    let input1 = input_element('td_'+row_length+'_'+j,'input_'+row_length+'_'+j,'',false,'white','white','normal','100%');
     tr1.appendChild(input1);
- }
- row_length++;
- return tr1;
+  }
+  //Id columns
+  tr1.appendChild(new_count_id('id_'+row_length,obj.id+'-'+row_length));
+  row_length++;
+  return tr1;
 }
 
+//Create header element
 function th_element(id,textContent, textTooltip){
   let th = document.createElement("th");
   th.id = id;
@@ -64,28 +66,52 @@ function th_element(id,textContent, textTooltip){
   return th;
 }
 
-  //Create header
+//Create count and id columns
+function new_count_id (id,innerhtml){
+    let td0 = document.createElement("td");
+    td0.id = id;
+    td0.innerHTML = innerhtml;
+    return td0;
+}
+
+  //Create headers:
   let container_header = document.getElementById("header1");
+
+  //1. First column is the count
+  let th0 = th_element("header_0","no",'');
+  container_header.appendChild(th0);
+
+  //2. The next is the user headings
   for (let i=0;i<obj.headers.length;i++){
       let th = th_element("header_"+ i,obj.headers[i],obj.headers_tooltip[i]);
       container_header.appendChild(th);
   }
 
-  //Insert values into table
+  //3. Finally the id header
+  let th_last = th_element("header_"+obj.headers.length,"id",'');
+  container_header.appendChild(th_last);
+
+  //Insert values into table body
   let container = document.getElementById("tbody1");
-  //Applies if obj.dataRows (saved rows) is empty
+
+  //This only applies if obj.dataRows (fetched rows) is empty, otherwise omitted
   if (obj.dataRows === undefined || obj.dataRows.length === 0) {
-    let tr1 = new_row(2,obj.headers.length);
+    let tr1 = new_row(obj.headers.length+2);
     container.appendChild(tr1);
   }
 
-  //This only applies if obj.dataRow (saved rows) is not empty, otherwise it generates nothing
-  for (let k=0;k<obj.dataRows.length;k++){
+  //This only applies if obj.dataRow (fetched rows) is not empty, otherwise it generates nothing
+  for (let k=1;k<(obj.dataRows.length+1);k++){
        let tr1 = document.createElement("tr");
-       for (let j=0;j<obj.dataRows[0].length;j++){
-          let input1 = input_element('td_'+k+'_'+j,'input_'+k+'_'+j,obj.dataRows[k][j],false,'white','white','normal','100%');
+       //First column is the count
+       tr1.appendChild(new_count_id("count_"+row_length,row_length));
+       //Second to almost last column is user info
+       for (let j=1;j<obj.dataRows[0].length+1;j++){
+          let input1 = input_element('td_'+k+'_'+j,'input_'+k+'_'+j,obj.dataRows[k-1][j-1],false,'white','white','normal','100%');
           tr1.appendChild(input1);
        }
+       //Last column is the id
+       tr1.appendChild(new_count_id("id_"+row_length,obj.id+"-"+row_length));
        row_length++;
        container.appendChild(tr1);
   }
@@ -127,6 +153,7 @@ function th_element(id,textContent, textTooltip){
   }*/
 }
 
+//Drag and drop - drag over event
 document.addEventListener("dragover", function( event ) {
       let id = event.target.id;
       //Don't copy yet, add id to drag_arr - drag_arr should only contain same values in a row
@@ -146,6 +173,7 @@ document.addEventListener("dragover", function( event ) {
      }
  }, false);
 
+ //Drag and drop - end of dragging
  document.addEventListener("dragend", function( event ) {
     //If drop_arr last value is equal to first value, skip everything.
     //The user has withdraw the action.
@@ -161,15 +189,18 @@ document.addEventListener("dragover", function( event ) {
     drag_arr = [];
  }, false);
 
+ //Drag and drop - disable drop
  document.addEventListener("drop", function( event ) {
    event.preventDefault();
  }, false);
 
 
+ //Drag and drop & select cell
  //Remove style borders from the whole table
+ //Useful when the user selects another cell
  function  remove_borders (){
-   for (let i=0;i<row_length;i++){
-      for (let j=0;j<col_length;j++){
+   for (let i=1;i<row_length;i++){
+      for (let j=1;j<col_length-1;j++){
          document.getElementById('td_'+ i + '_' + j).classList.remove('selectCell');
          document.getElementById('td_'+ i + '_' + j).classList.remove('dragCell');
       }
@@ -179,15 +210,17 @@ document.addEventListener("dragover", function( event ) {
 
 //Set cell and row select
 let handleClick = function (event) {
-  let doc = document.getElementById(event.explicitOriginalTarget.id);
-  let elem = doc.parentElement;
-  //Remove borders from the whole table
-  remove_borders();
-  //Mark selected cell
-  elem.classList.add("selectCell");
-  //Make it draggable
-  elem.draggable = "true";
-  elem.ondragstart = addEventListener('dragstart', function(event) { event.dataTransfer.setData('text/plain', doc.value); });
+  if ((event.explicitOriginalTarget.id).startsWith('input') || (event.explicitOriginalTarget.id).startsWith('select')) {
+      let doc = document.getElementById(event.explicitOriginalTarget.id);
+      let elem = doc.parentElement;
+      //Remove borders from the whole table
+      remove_borders();
+      //Mark selected cell
+      elem.classList.add("selectCell");
+      //Make it draggable
+      elem.draggable = "true";
+      elem.ondragstart = addEventListener('dragstart', function(event) { event.dataTransfer.setData('text/plain', doc.value); });
+}
 }
 
 // new button pressed
